8bb5a587aa023969f3e01881103e19a57b29ac7c
Robin Lifshitz <Rlifshitz19@gmail.com>
2024-04-04 20:45:33 -0700

    Added Ai Controller and Ai Anim Instance Base Classes

---
+++ Source/timbermvp/Public/AI/TimberAiAnimInstanceBase.h
@@ -0,0 +1,17 @@
+// Property of Paracosm Industries. Dont use my shit.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "TimberAiAnimInstanceBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TIMBERMVP_API UTimberAiAnimInstanceBase : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/timbermvp/Public/AI/TimberAiControllerBase.h
@@ -0,0 +1,17 @@
+// Property of Paracosm Industries. Dont use my shit.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "TimberAiControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TIMBERMVP_API ATimberAiControllerBase : public AAIController
+{
+	GENERATED_BODY()
+	
+};

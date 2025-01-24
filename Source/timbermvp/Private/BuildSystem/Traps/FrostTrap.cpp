bf63b75d62839e2d342ba2eedab2f08c76a29988
Robin Lifshitz <Rlifshitz19@gmail.com>
2025-01-24 10:03:43 -0800

    Not rebuilding the Build System, Will manually rotate the Meshes if necessary. Rebuild after GDC. Frost Trap BP Added to Buildables.

---
+++ Source/timbermvp/Private/BuildSystem/Traps/FrostTrap.cpp
@@ -0,0 +1,30 @@
+﻿// Property of Paracosm Industries. Dont use my shit.
+
+
+#include "BuildSystem/Traps/FrostTrap.h"
+
+
+// Sets default values
+AFrostTrap::AFrostTrap()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	FrostTrapVentMesh = CreateDefaultSubobject<UStaticMeshComponent>("Frost Trap Vent");
+	FrostTrapVentMesh->SetupAttachment(RootComponent);
+	
+}
+
+// Called when the game starts or when spawned
+void AFrostTrap::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFrostTrap::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/timbermvp/Public/BuildSystem/Traps/FrostTrap.h
@@ -0,0 +1,28 @@
+﻿// Property of Paracosm Industries. Dont use my shit.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TrapBase.h"
+#include "FrostTrap.generated.h"
+
+UCLASS()
+class TIMBERMVP_API AFrostTrap : public ATrapBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AFrostTrap();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	UStaticMeshComponent* FrostTrapVentMesh;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};

cd908623220a07088b5e27f05269e05b069a69c7
Robin Lifshitz <Rlifshitz19@gmail.com>
2025-02-06 12:03:24 -0800

    Health Bar Widget for Seeda Added. Works as expected.

--- Source/timbermvp/Private/Character/TimberPlayableCharacter.cpp
+++ Source/timbermvp/Private/Character/TimberPlayableCharacter.cpp
@@ -85,6 +85,7 @@ void ATimberPlayableCharacter::BindToSeedaDelegates(AActor* Seeda)
 
 		//When seeda dies, handle player death
 		SeedaRef->OnSeedaDeath.AddDynamic(this, &ATimberPlayableCharacter::HandlePlayerDeath);
+		
 		//When Seeda is being Destroyed
 		SeedaRef->OnDestroyed.AddDynamic(this, &ATimberPlayableCharacter::UnbindFromSeedaDeathDelegate);
 	}

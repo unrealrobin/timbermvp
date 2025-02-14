01d30c61a6b11fba1e8026db5d7b6448104f6536
Robin Lifshitz <Rlifshitz19@gmail.com>
2025-02-14 11:10:38 -0800

    Created Unique Loot Drop. Works as expected. This is the Rarest Loot drop for the best Traps and Constructs.

---
+++ Source/timbermvp/Private/Loot/LootUnique.cpp
@@ -0,0 +1,50 @@
+﻿// Property of Paracosm Industries. Dont use my shit.
+
+
+#include "Loot/LootUnique.h"
+
+#include "Character/TimberPlayableCharacter.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/Inventory/InventoryManagerComponent.h"
+
+
+class ATimberPlayableCharacter;
+// Sets default values
+ALootUnique::ALootUnique()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ALootUnique::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CollisionCapsule->OnComponentBeginOverlap.AddDynamic(this, &ALootUnique::HandleLootItemOverlap);
+	
+}
+
+// Called every frame
+void ALootUnique::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ALootUnique::HandleLootItemOverlap(
+	UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	bool bFromSweep, const FHitResult& SweepResult)
+{
+	//UE_LOG(LogTemp, Warning, TEXT("Overlapped Part"));
+	ATimberPlayableCharacter* PlayerCharacter = Cast<ATimberPlayableCharacter>(OtherActor);
+
+	if(PlayerCharacter)
+	{
+		// Add parts to player Inventory
+		PlayerCharacter->InventoryManager->AddUniquesToInventory(1);
+
+		// Destroy the parts item
+		Destroy();
+	}
+}
+
---
+++ Source/timbermvp/Public/Loot/LootUnique.h
@@ -0,0 +1,28 @@
+﻿// Property of Paracosm Industries. Dont use my shit.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyLootDropBase.h"
+#include "LootUnique.generated.h"
+
+UCLASS()
+class TIMBERMVP_API ALootUnique : public AEnemyLootDropBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ALootUnique();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void HandleLootItemOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+};

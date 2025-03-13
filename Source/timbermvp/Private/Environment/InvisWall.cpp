be2f45745d21bcbb99156a71be3dfc4cf7468b84
Robin Lifshitz <Rlifshitz19@gmail.com>
2025-03-12 21:49:26 -0700

    Invis Wall added to level. Invis Walls keep Player Character from moving past the lab door area. Invis Walls allows all Enemy Character to move freely through and only keeps the player out. Enemy characters must Ignor InvisWall Custom Object Channel.

---
+++ Source/timbermvp/Private/Environment/InvisWall.cpp
@@ -0,0 +1,31 @@
+﻿// Property of Paracosm Industries. Dont use my shit.
+
+
+#include "Environment/InvisWall.h"
+
+
+// Sets default values
+AInvisWall::AInvisWall()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootSceneComponent = CreateDefaultSubobject<USceneComponent>("RootSceneComponent");
+	RootComponent = RootSceneComponent;
+	WallStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("StaticMesh");
+	WallStaticMesh->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void AInvisWall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AInvisWall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/timbermvp/Public/Environment/InvisWall.h
@@ -0,0 +1,30 @@
+﻿// Property of Paracosm Industries. Dont use my shit.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "InvisWall.generated.h"
+
+UCLASS()
+class TIMBERMVP_API AInvisWall : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AInvisWall();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	USceneComponent* RootSceneComponent;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* WallStaticMesh;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};

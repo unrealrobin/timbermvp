1c6496e8ebc6f1a7d8429cd714688e124f386dd3
Robin Lifshitz <Rlifshitz19@gmail.com>
2025-04-04 13:18:40 -0700

    Created a Dynamic Lab BP that Generates the Lab based on Length/width/height described in BP. Right now only the floor portion is working. We are using ChildActor Components and adding them to the Lab BP, We call the BuildLab() function in the OnConstruction Scripts so we can see and update the lab in editor without having to press play.

---
+++ Source/timbermvp/Private/Environment/DynamicLab.cpp
@@ -0,0 +1,116 @@
+﻿// Property of Paracosm Industries. Dont use my shit.
+#include "Environment/DynamicLab.h"
+
+
+
+// Sets default values
+ADynamicLab::ADynamicLab()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ADynamicLab::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ADynamicLab::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	BuildLab();
+}
+
+// Called every frame
+void ADynamicLab::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ADynamicLab::BuildLab()
+{
+	EmptyChildComponentArray();
+	HandleFloorLayoutChildComponent();
+}
+
+void ADynamicLab::HandleFloorLayout()
+{
+	//Looping through Length -> +X Direction
+	for (int i = 0; i <= LabLength -1; i++ )
+	{
+		for (int k = 0; k <= LabWidth -1; k++ )
+		{
+			if (EnvironmentFloors)
+			{
+				int LengthIncrement = i; //(0,1,2,3,4)
+				int WidthIncrement = k; //(0,1,2,3,4)
+				FVector FloorSpawnLocation = FVector::ZeroVector;
+				FloorSpawnLocation.X = LengthIncrement * FloorSizeX;
+				FloorSpawnLocation.Y = WidthIncrement * FloorSizeY;
+
+				//Something
+			}
+		}
+	}
+}
+
+void ADynamicLab::EmptyChildComponentArray()
+{
+	if (ChildComponents.Num() > 0)
+	{
+		for (UChildActorComponent* ChildActor : ChildComponents)
+		{
+			if (ChildActor)
+			{
+				ChildActor->DestroyComponent();
+			}
+		}
+		ChildComponents.Empty();
+	}
+}
+
+void ADynamicLab::HandleFloorLayoutChildComponent()
+{
+	//Looping through Length -> +X Direction
+	for (int i = 0; i <= LabLength -1; i++ )
+	{
+		//Looping through Width -> +Y Direction
+		for (int k = 0; k <= LabWidth -1; k++ )
+		{
+			//If the EnvironmentFloors Class is set
+			if (EnvironmentFloors)
+			{
+				int LengthIncrement = i; //(0,1,2,3,4)
+				int WidthIncrement = k; //(0,1,2,3,4)
+				FVector FloorSpawnLocation = FVector::ZeroVector;
+				FloorSpawnLocation.X = LengthIncrement * FloorSizeX;
+				FloorSpawnLocation.Y = WidthIncrement * FloorSizeY;
+
+				GenerateChildComponent(EnvironmentFloors, FloorSpawnLocation, FRotator::ZeroRotator);
+				
+			}
+		}
+	}
+
+	NumberOfFloors = ChildComponents.Num();
+}
+
+void ADynamicLab::GenerateChildComponent(TSubclassOf<AActor> BuildableActor, FVector Location, FRotator Rotation)
+{
+	//Create the New Component
+	UChildActorComponent* ChildActorComponent = NewObject<UChildActorComponent>(this,  UChildActorComponent::StaticClass());
+	//Attach it to the Root Component of the BP_Lab
+	ChildActorComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	//Set the Child Actor Class - This Can be the BP of Walls, Floor, Lights, Doors Etc.
+	ChildActorComponent->SetChildActorClass(BuildableActor);
+	//Set Location
+	ChildActorComponent->SetRelativeLocation(Location);
+	//Set Rotation
+	ChildActorComponent->SetRelativeRotation(Rotation);
+	ChildActorComponent->RegisterComponent();
+
+	ChildComponents.Add(ChildActorComponent);
+	
+}
---
+++ Source/timbermvp/Public/Environment/DynamicLab.h
@@ -0,0 +1,79 @@
+﻿// Property of Paracosm Industries. Dont use my shit.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DynamicLab.generated.h"
+
+class ABuildableBase;
+
+UCLASS()
+class TIMBERMVP_API ADynamicLab : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ADynamicLab();
+
+private:
+	UFUNCTION()
+	void HandleFloorLayout();
+	void EmptyChildComponentArray();
+
+	void BuildLab();
+	
+	void HandleFloorLayoutChildComponent();
+
+	void GenerateChildComponent(TSubclassOf<AActor> BuildableActor, FVector Location, FRotator Rotation);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Lab Details")
+	TArray<UChildActorComponent*> ChildComponents;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Lab Details")
+	int NumberOfFloors = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lab Meshes")
+	TSubclassOf<AActor> EnvironmentWallsClass = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lab Meshes")
+	TSubclassOf<AActor> EnvironmentFloors = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lab Meshes")
+	TSubclassOf<AActor> LabDoors = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lab Meshes")
+	TSubclassOf<AActor> GarageDoor = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lab Meshes")
+	TSubclassOf<AActor> CeilingLights = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lab Meshes")
+	float FloorSizeX = 395.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lab Meshes")
+	float FloorSizeY = 395.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lab Details")
+	int LabFloors = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lab Details")
+	int LabLength = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lab Details")
+	int LabWidth = 1;
+
+
+	
+};

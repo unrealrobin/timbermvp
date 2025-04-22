36b63cd42e12a870aa759370f98e2b2b3595408f
Robin Lifshitz <Rlifshitz19@gmail.com>
2025-04-21 20:37:14 -0700

    Building Enemy Drone Character.

---
+++ Source/timbermvp/Private/Character/Enemies/EnemyDrone.cpp
@@ -0,0 +1,26 @@
+﻿// Property of Paracosm.
+
+
+#include "Character/Enemies/EnemyDrone.h"
+
+
+// Sets default values
+AEnemyDrone::AEnemyDrone()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AEnemyDrone::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEnemyDrone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/timbermvp/Public/Character/Enemies/EnemyDrone.h
@@ -0,0 +1,26 @@
+﻿// Property of Paracosm.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TimberEnemyCharacter.h"
+#include "EnemyDrone.generated.h"
+
+UCLASS()
+class TIMBERMVP_API AEnemyDrone : public ATimberEnemyCharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AEnemyDrone();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+};

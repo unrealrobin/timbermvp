43990db9425e0cf77ce248797539bb038200056c
Robin Lifshitz <Rlifshitz19@gmail.com>
2025-06-19 15:01:57 -0700

    Added Frost VFX Asset Pack. Add VFX for Frost Trap blowing.

--- Source/timbermvp/Private/BuildSystem/Traps/BurnTrap.cpp
+++ Source/timbermvp/Private/BuildSystem/Traps/BurnTrap.cpp
@@ -3,6 +3,7 @@
 
 #include "BuildSystem/Traps/BurnTrap.h"
 
+#include "NiagaraComponent.h"
 #include "Components/BoxComponent.h"
 #include "Data/DataAssets/StatusEffects/StatusEffectBase.h"
 
@@ -16,22 +17,45 @@ ABurnTrap::ABurnTrap()
 	
 	BurnTrapInternalsMesh = CreateDefaultSubobject<UStaticMeshComponent>("BurnTrapInternalsMesh");
 	BurnTrapInternalsMesh->SetupAttachment(TrapBaseStaticMesh);
+
+	BurnTrapInternalsNiagara = CreateDefaultSubobject<UNiagaraComponent>("BurnTrapInternalsNiagara");
+	BurnTrapInternalsNiagara->SetupAttachment(BurnTrapInternalsMesh);
 	
 	HitBoxComponent->OnComponentBeginOverlap.AddDynamic(this, &ABurnTrap::HandleTrapBeginOverlap);
+	HitBoxComponent->OnComponentEndOverlap.AddDynamic(this, &ABurnTrap::HandleTrapEndOverlap);
+	
 }
 
 // Called when the game starts or when spawned
 void ABurnTrap::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 void ABurnTrap::HandleTrapBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (BurnTrapInternalsNiagara)
+	{
+		BurnTrapInternalsNiagara->ActivateSystem();
+	}
+	
 	if (StatusEffectDataAsset)
 	{
 		AddEffectToEnemy(OtherActor, StatusEffectDataAsset->StatusEffect);
+	}
+}
+
+void ABurnTrap::HandleTrapEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 INT32)
+{
+	//Check if anyone is still in hitbox, if not, turn off the effect.
+	if (InsideHitBoxArray.Num() == 0)
+	{
+		if (BurnTrapInternalsNiagara)
+		{
+			BurnTrapInternalsNiagara->Deactivate();
+			UE_LOG(LogTemp, Display, TEXT("Burn Trap - Deactivated Niagara System."));
+		}
 	}
 }
 
--- Source/timbermvp/Public/BuildSystem/Traps/BurnTrap.h
+++ Source/timbermvp/Public/BuildSystem/Traps/BurnTrap.h
@@ -6,13 +6,15 @@
 #include "TrapBase.h"
 #include "BurnTrap.generated.h"
 
+class UNiagaraComponent;
+
 UCLASS()
 class TIMBERMVP_API ABurnTrap : public ATrapBase
 {
 	GENERATED_BODY()
 
 public:
-	
+
 	// Sets default values for this actor's properties
 	ABurnTrap();
 
@@ -25,6 +27,9 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
 	float InternalsSpinTime = 3.0f;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VFX")
+	UNiagaraComponent* BurnTrapInternalsNiagara = nullptr;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -32,6 +37,8 @@ protected:
 	UFUNCTION()
 	void HandleTrapBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	UFUNCTION()
+	void HandleTrapEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 INT32);	
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;

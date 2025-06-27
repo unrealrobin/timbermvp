2c7f7bbbe56a24928e75045cc50e586d120b9d5d
Robin Lifshitz <Rlifshitz19@gmail.com>
2025-06-26 22:00:43 -0700

    Working on new Path Finding Solution. Created new Test BP/Controller/BT/BB

--- Source/timbermvp/Public/Components/Navigation/NavigationHelperComponent.h
+++ Source/timbermvp/Public/Components/Navigation/NavigationHelperComponent.h
@@ -10,6 +10,37 @@
 class ABuildableBase;
 class ATimberCharacterBase;
 
+UENUM(BlueprintType)
+enum class EPathFailureReason : uint8
+{
+	None,
+	NoPathExists,
+	Blocked,
+	TargetUnreachable
+};
+
+//Struct for Path Finding Result
+USTRUCT(BlueprintType)
+struct FPathResult
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly, Category = "Path Result")
+	TArray<FVector> PathPoints;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Path Result")
+	bool bIsValid = false;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Path Result")
+	bool bIsPartial = false;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Path Result")
+	FVector ClosestAccessiblePoint = FVector::ZeroVector;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Path Result")
+	EPathFailureReason FailureReason = EPathFailureReason::None;
+};
+
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class TIMBERMVP_API UNavigationHelperComponent : public UActorComponent
 {
@@ -31,16 +62,23 @@ protected:
 
 public:
 
-	FVector GetCenterOfNode(NavNodeRef Node);
+	//Path Calculations with Fallbacks
+	UFUNCTION(BlueprintCallable, Category = "Navigation Logic")
+	FPathResult CalculatePathToTarget(FVector Start, FVector End);
 
+	//Find Closest Accessible point when no direct path exists.
+	UFUNCTION(BlueprintCallable, Category = "Navigation Logic")
+	FVector FindClosestAccessiblePoint(FVector Start, FVector Target);
+	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Navigation Data")
 	bool bIsLastPathPartial = false;
 	
-	FVector LastPathPoint = FVector::ZeroVector;
-
 	//Uses UE Nav Path and adjusts it to be centered on Polys.
 	UFUNCTION(BlueprintCallable, Category = "Navigation Logic")
 	TArray<FVector> GetCorridorPathPoints(FVector Start, FVector End);
 
+	FVector GetCenterOfNode(NavNodeRef Node);
+	FVector LastPathPoint = FVector::ZeroVector;
 	bool CheckIfPathShouldUpdate(ABuildableBase* BuildableActor);
+	
 };

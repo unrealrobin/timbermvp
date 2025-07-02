4f33b4f643e2b8a72e44d0d56d9bdb07f992be86
Robin Lifshitz <Rlifshitz19@gmail.com>
2025-07-02 09:59:48 -0700

    Created Custom DieRobotGameInstance and DieRobotGameUserSettings. Game now defaults to 1920x1080. These classes will be used in the future for User applied settings etc.

---
+++ Source/timbermvp/Private/Settings/DieRobotGameUserSettings.cpp
@@ -0,0 +1,17 @@
+﻿// Property of Paracosm.
+
+
+#include "Settings/DieRobotGameUserSettings.h"
+
+void UDieRobotGameUserSettings::ApplySettings(bool bCheckForCommandLineOverrides)
+{
+
+	SetScreenResolution(FIntPoint(1920, 1080));
+	SetFullscreenMode(EWindowMode::Windowed);
+
+	ConfirmVideoMode();
+	
+	Super::ApplySettings(bCheckForCommandLineOverrides);
+
+	SaveSettings();
+}

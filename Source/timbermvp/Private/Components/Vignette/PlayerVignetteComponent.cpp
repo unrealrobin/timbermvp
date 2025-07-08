67f35713664eee3c3ab58c35ec4069e9e6d6c094
Robin Lifshitz <Rlifshitz19@gmail.com>
2025-07-07 21:35:39 -0700

    Working on Vignette Actor Component. Creating Dynamic Vignette that adjusts to the player health upon taking or gaining life.

---
+++ Source/timbermvp/Private/Components/Vignette/PlayerVignetteComponent.cpp
@@ -0,0 +1,103 @@
+﻿// Property of Paracosm.
+
+
+#include "Components/Vignette/PlayerVignetteComponent.h"
+
+#include "Components/PostProcessComponent.h"
+
+
+// Sets default values for this component's properties
+UPlayerVignetteComponent::UPlayerVignetteComponent()
+{
+
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+// Called when the game starts
+void UPlayerVignetteComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AActor* Owner = GetOwner();
+	if (!Owner) return;
+
+	VignettePostProcessComponent = NewObject<UPostProcessComponent>();
+	if (!VignettePostProcessComponent) return;
+
+	VignettePostProcessComponent->RegisterComponent();
+	VignettePostProcessComponent->bUnbound = true;
+
+	USceneComponent* ActorRootComponent = Owner->GetRootComponent();
+	if (ActorRootComponent)
+	{
+		VignettePostProcessComponent->AttachToComponent(ActorRootComponent, FAttachmentTransformRules::KeepRelativeTransform);	
+	}
+
+	InitializeVignetteDynamicMaterial();
+}
+
+FDamageVignetteState UPlayerVignetteComponent::GetCurrentDamageVignetteState()
+{
+	FDamageVignetteState OutState = FDamageVignetteState();
+	
+	if (DamageVignetteMid)
+	{
+		FHashedMaterialParameterInfo DamageIntensityParam(TEXT("DamageIntensity"));
+		FHashedMaterialParameterInfo EdgeFalloffParam(TEXT("EdgeFalloff"));
+		FHashedMaterialParameterInfo DamageColorParam(TEXT("DamageColor"));
+		
+		DamageVignetteMid->GetScalarParameterValue(DamageIntensityParam, OutState.DamageIntensity);
+		DamageVignetteMid->GetScalarParameterValue(EdgeFalloffParam, OutState.EdgeFalloff);
+		DamageVignetteMid->GetVectorParameterValue(DamageColorParam, OutState.VignetteColor);
+	}
+	
+	return OutState;
+}
+
+void UPlayerVignetteComponent::InitializeVignetteDynamicMaterial()
+{
+	if (DamageVignetteMaterialBase)
+	{
+		DamageVignetteMid = UMaterialInstanceDynamic::Create(DamageVignetteMaterialBase, this);
+	}
+}
+
+void UPlayerVignetteComponent::HandleHealthChange(float HealthPercent)
+{
+	//TODO:: Called when player takes damage or gains health.
+	
+	//CHecks health percent
+	CurrentState = GetCurrentDamageVignetteState();
+
+	if (FMath::IsWithin(HealthPercent, .8, 1.0))
+	{
+		LerpDamageVignetteState(CurrentState, FullState, 1.0f);
+	}
+	else if (FMath::IsWithin(HealthPercent, .5, .8))
+	{
+		LerpDamageVignetteState(CurrentState, LightState, 1.0f);
+	}
+	else if (FMath::IsWithin(HealthPercent, .25, .5))
+	{
+		LerpDamageVignetteState(CurrentState, HighState, 1.0f);
+	}
+	else if (FMath::IsWithin(HealthPercent, 0.0, .25))
+	{
+		LerpDamageVignetteState(CurrentState, CriticalState, 1.0f);
+	}
+	else
+	{
+		//Edge Case. Doesnt show any Vignette, like full Health.
+		LerpDamageVignetteState(CurrentState, FullState, 1.0f);
+	}
+	
+}
+
+void UPlayerVignetteComponent::LerpDamageVignetteState(FDamageVignetteState From, FDamageVignetteState To, float Time)
+{
+	FTimerHandle LerpTimerHandle;
+
+	//Handle getting Alpha and lerping From -> To states.
+}
+
---
+++ Source/timbermvp/Public/Components/Vignette/PlayerVignetteComponent.h
@@ -0,0 +1,96 @@
+﻿// Property of Paracosm.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PlayerVignetteComponent.generated.h"
+
+UENUM(BlueprintType)
+enum EDamageVignetteState
+{
+	FullHealth UMETA(DisplayName = "100%"),
+	LightDamage UMETA(DisplayName = "80%"),
+	HighDamage UMETA(DisplayName = "50%"),
+	Critical UMETA(DisplayName = "25%"),
+	Default UMETA(DisplayName = "Default"),
+};
+
+USTRUCT(BlueprintType)
+struct FDamageVignetteState
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DamageIntensity = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float EdgeFalloff = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FLinearColor VignetteColor = FLinearColor::Black;
+
+	FDamageVignetteState() {}
+
+	FDamageVignetteState(float InIntensity, float InFalloff, FLinearColor InColor)
+		: DamageIntensity(InIntensity), EdgeFalloff(InFalloff), VignetteColor(InColor)
+	{}
+};
+
+
+
+class UPostProcessComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TIMBERMVP_API UPlayerVignetteComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UPlayerVignetteComponent();
+
+	//Where the Vignette gets added and modified.
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UPostProcessComponent* VignettePostProcessComponent = nullptr;
+
+	//Stores the Base Material used for the Vignette.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Materials")
+	UMaterialInterface* DamageVignetteMaterialBase = nullptr;
+
+	//Not Assignable in Editor. Required for Adjusting Params at Runtime.
+	UPROPERTY()
+	UMaterialInstanceDynamic* DamageVignetteMid = nullptr;
+
+	//Tracks the Current State of the Vignette.
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Vignette")
+	EDamageVignetteState CurrentVignetteState = FullHealth;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage Vignette State")
+	FDamageVignetteState CurrentState = FDamageVignetteState(0.0f, 0.0f, FLinearColor::Red);
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage Vignette State")
+	FDamageVignetteState FullState = FDamageVignetteState(0.0f, 0.0f, FLinearColor::Red);
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage Vignette State")
+	FDamageVignetteState LightState = FDamageVignetteState(2.0f, 5.0f, FLinearColor::Red);
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage Vignette State")
+	FDamageVignetteState HighState = FDamageVignetteState(4.0f, 4.0f, FLinearColor::Red);
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage Vignette State")
+	FDamageVignetteState CriticalState = FDamageVignetteState(5.0f, 3.0f, FLinearColor::Red);
+
+	FDamageVignetteState GetCurrentDamageVignetteState();
+private:
+
+	UFUNCTION()
+	void InitializeVignetteDynamicMaterial();
+
+	UFUNCTION()
+	void HandleHealthChange(float HealthPercent);
+
+	void LerpDamageVignetteState(FDamageVignetteState From, FDamageVignetteState To, float Time);
+	
+	
+};

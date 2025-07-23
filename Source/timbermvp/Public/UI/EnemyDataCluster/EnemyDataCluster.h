60b6f9fb31983862fbb6cc12be5cbe50995f8da6
Robin Lifshitz <Rlifshitz19@gmail.com>
2025-07-22 18:29:47 -0700

    DR-50: Synergy System V1 - Basics in Place and Ready for Exapansion and Polish.

---
+++ Source/timbermvp/Public/UI/EnemyDataCluster/EnemyDataCluster.h
@@ -0,0 +1,25 @@
+﻿// Property of Paracosm.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EnemyDataCluster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TIMBERMVP_API UEnemyDataCluster : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health Bar")
+	UUserWidget* HealthBarWidget;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health Bar")
+	UUserWidget* StatusEffectBarWidget;
+	
+};
